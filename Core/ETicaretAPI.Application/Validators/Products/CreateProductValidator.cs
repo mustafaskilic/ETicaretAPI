f9f3325d421ea3f96f48d887b5c1630445d52653
mustafakilic <mustafasuheylkilic@gmail.com>
2022-10-10 00:15:46 +0300

    api continue

---
+++ Core/ETicaretAPI.Application/Validators/Products/CreateProductValidator.cs
@@ -0,0 +1,40 @@
+﻿using ETicaretAPI.Application.ViewModels.Products;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETicaretAPI.Application.Validators.Products
+{
+    public class CreateProductValidator : AbstractValidator<VM_Create_Product>
+    {
+        public CreateProductValidator()
+        {
+
+            RuleFor(p => p.Name)
+                .NotEmpty()
+                .NotNull()
+                    .WithMessage("Lütfen ürün adını boş geçmeyiniz.")
+                    .MaximumLength(150)
+                    .MinimumLength(2)
+                    .WithMessage($"Lütfen ürün adını 2 ile 150 arasında giriniz");
+
+            RuleFor(p => p.Stock)
+                .NotEmpty()
+                .NotNull()
+                    .WithMessage("Lütfen stok bilgisini boş girmeyin")
+                    .Must(s => s >= 0)
+                        .WithMessage("Stok bilgisi negatif olamaz!");
+
+            RuleFor(p => p.Price)
+              .NotEmpty()
+              .NotNull()
+                  .WithMessage("Lütfen fiyat bilgisini boş girmeyin")
+                  .Must(s => s >= 0)
+                      .WithMessage("Fiyat bilgisi negatif olamaz!");
+        }
+    }
+}
